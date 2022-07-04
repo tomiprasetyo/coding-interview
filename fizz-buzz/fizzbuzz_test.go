4ba33c0c47522081b727e8fe7ef72de8282d0ee7
Tomi Prasetyo <tomiprasetyo95@gmail.com>
2022-07-04 17:04:55 +0700

    fizz buzz

---
+++ fizz-buzz/fizzbuzz_test.go
@@ -0,0 +1,26 @@
+package test
+
+import (
+	"fmt"
+	"testing"
+)
+
+func fizzBuzz(total int) {
+	var i int
+	for i = 1; i <= total; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("fizz buzz")
+		} else if i%3 == 0 {
+			fmt.Println("fizz")
+		} else if i%5 == 0 {
+			fmt.Println("buzz")
+		} else {
+			fmt.Println(i)
+		}
+
+	}
+}
+
+func TestFizzBuzz(t *testing.T) {
+	fizzBuzz(100)
+}
